a9ff09450d4545492e9f47d7e64b6dceb194be10
jvfnjnwnr <73676681+jvfnjnwnr@users.noreply.github.com>
2022-10-14 11:49:23 +1000

    Update stats.py

--- ssg/extensions/stats.py
+++ ssg/extensions/stats.py
@@ -6,10 +6,11 @@ global total_written
 total_written = 0
 @hooks.register("start_build")
 def start_build():
-    global start_time = time.now()
+    global start_time
+    start_time = time.now()
 @hooks.register("written")
 def written():
-    global total_written += 1
+    total_written += 1
 @hooks.register("stats")
 def stats():
     final_time = time.now() - start_time
