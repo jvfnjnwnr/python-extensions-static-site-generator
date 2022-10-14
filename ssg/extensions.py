1c956c936c85f9adea05ef557fb0e87e57048918
jvfnjnwnr <73676681+jvfnjnwnr@users.noreply.github.com>
2022-10-14 10:53:26 +1000

    Update extensions.py

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -5,8 +5,8 @@ def load_module(directory, name):
     sys.path.insert(0, directory)
     importlib.import_module(name)
     sys.path.pop(0)
-def load_directory(directory: Path):
-    for path in directory.rglob(path, "*.py"):
+def load_directory(directory):
+    for path in directory.rglob("*.py"):
         load_module(directory.as_posix(), path.stem)
 def load_bundled():
     directory = Path(__file__).parent / "extensions"
